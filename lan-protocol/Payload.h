1e8ca1144a4eacb90ee1f71e01f7f1277f184e85
Kieron Woodhouse <kieron.woodhouse@yahoo.co.uk>
2016-03-03 17:08:14 +0000

    Added << operators to all device message payloads.

--- lan-protocol/Payload.h
+++ lan-protocol/Payload.h
@@ -47,7 +47,7 @@ struct StateService : public DevicePayload
 
     friend std::ostream& operator<< (std::ostream& os, const StateService& payload)
     {
-        os << payload.service << "."
+        os << payload.service
            << payload.port;
         return os;
     }
@@ -63,6 +63,15 @@ struct StateHostInfo : public DevicePayload
     uint32_t    tx;
     uint32_t    rx;
     int16_t     reserved0;
+
+    friend std::ostream& operator<< (std::ostream& os, const StateHostInfo& payload)
+    {
+        os << payload.signal
+           << payload.tx
+           << payload.rx
+           << payload.reserved0;
+        return os;
+    }
 };
 
 struct GetHostFirmware : public DevicePayload
@@ -74,6 +83,14 @@ struct StateHostFirmware : public DevicePayload
     uint64_t    build;
     uint64_t    reserved0;
     uint32_t    version;
+
+    friend std::ostream& operator<< (std::ostream& os, const StateHostFirmware& payload)
+    {
+        os << payload.build
+           << payload.reserved0
+           << payload.version;
+        return os;
+    }
 };
 
 struct GetWifiInfo : public DevicePayload
@@ -86,6 +103,15 @@ struct StateWifiInfo : public DevicePayload
     uint32_t    tx;
     uint32_t    rx;
     int16_t     reserved0;
+
+    friend std::ostream& operator<< (std::ostream& os, const StateWifiInfo& payload)
+    {
+        os << payload.signal
+           << payload.tx
+           << payload.rx
+           << payload.reserved0;
+        return os;
+    }
 };
 
 struct GetWifiFirmware : public DevicePayload
@@ -97,6 +123,14 @@ struct StateWifiFirmware : public DevicePayload
     uint64_t    build;
     uint64_t    reserved0;
     uint32_t    version;
+
+    friend std::ostream& operator<< (std::ostream& os, const StateWifiFirmware& payload)
+    {
+        os << payload.build
+           << payload.reserved0
+           << payload.version;
+        return os;
+    }
 };
 
 struct GetPower : public DevicePayload
@@ -111,6 +145,12 @@ struct SetPower : public DevicePayload
 struct StatePower : public DevicePayload
 {
     level_t     level;
+
+    friend std::ostream& operator<< (std::ostream& os, const StatePower& payload)
+    {
+        os << payload.level;
+        return os;
+    }
 };
 
 struct GetLabel : public DevicePayload
@@ -125,6 +165,13 @@ struct SetLabel : public DevicePayload
 struct StateLabel : public DevicePayload
 {
     label_t     label;
+
+    friend std::ostream& operator<< (std::ostream& os, const StateLabel& payload)
+    {
+        //TODO: test proper output of label payload (not null-terminated).
+        os << payload.label;
+        return os;
+    }
 };
 
 struct GetVersion : public DevicePayload
@@ -136,6 +183,14 @@ struct StateVersion : public DevicePayload
     uint32_t    vendor;
     uint32_t    product;
     uint32_t    version;
+
+    friend std::ostream& operator<< (std::ostream& os, const StateVersion& payload)
+    {
+        os << payload.vendor
+           << payload.product
+           << payload.version;
+        return os;
+    }
 };
 
 struct GetInfo : public DevicePayload
@@ -147,6 +202,14 @@ struct StateInfo : public DevicePayload
     time_t      time;
     uint64_t    uptime;
     uint64_t    downtime;
+
+    friend std::ostream& operator<< (std::ostream& os, const StateInfo& payload)
+    {
+        os << payload.time
+           << payload.uptime
+           << payload.downtime;
+        return os;
+    }
 };
 
 struct Acknowledgement : public DevicePayload
@@ -162,6 +225,14 @@ struct StateLocation : public DevicePayload
     uint8_t     location[16];
     label_t     label;
     uint64_t    updated_at;
+
+    friend std::ostream& operator<< (std::ostream& os, const StateLocation& payload)
+    {
+        os << payload.location
+           << payload.label
+           << payload.updated_at;
+        return os;
+    }
 };
 
 struct GetGroup : public DevicePayload
@@ -172,7 +243,15 @@ struct StateGroup : public DevicePayload
 {
     uint8_t     group[16];
     label_t     label;
-    uint64_t    update_at;
+    uint64_t    updated_at;
+
+    friend std::ostream& operator<< (std::ostream& os, const StateGroup& payload)
+    {
+        os << payload.group
+           << payload.label
+           << payload.updated_at;
+        return os;
+    }
 };
 
 struct EchoRequest : public DevicePayload
@@ -183,6 +262,12 @@ struct EchoRequest : public DevicePayload
 struct EchoResponse : public DevicePayload
 {
     uint8_t     payload[64];
+
+    friend std::ostream& operator<< (std::ostream& os, const EchoResponse& payload)
+    {
+        os << payload.payload;
+        return os;
+    }
 };
 
 } // namespace DevicePayload
