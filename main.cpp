c25cfea80c8b95e47c10b2d0e08ea6a16d50f739
Kieron Woodhouse <kieron.woodhouse@yahoo.co.uk>
2016-03-03 16:30:54 +0000

    Temporary main function to enable testing and debugging

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
+#include "lan-protocol/Device.h"
+#include "network/BoostUdp.h"
+
+#include <boost/asio.hpp>
+
 int main (int argc, char* argv[])
 {
+    (void)argc; (void)argv;
+
+    DevicePayload::GetService payload;
+    Device::GetService(payload);
+//    boost::asio::io_service ios;
+//    BoostUdp client (ios, "localhost", "1337");
+
+//    client.Send("Hello world\r\n");
+
     return 0;
 }
