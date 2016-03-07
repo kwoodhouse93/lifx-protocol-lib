4afddf722072dc1529f4c2ca6efd660b69b9972e
Kieron Woodhouse <kieron.woodhouse@yahoo.co.uk>
2016-03-07 17:46:47 +0000

    Updated main function for debugging purposes

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include "lan-protocol/Device.h"
-#include "network/BoostUdp.h"
 
 #include <boost/asio.hpp>
+
+// Debugging
+#include <iostream>
 
 int main (int argc, char* argv[])
 {
     (void)argc; (void)argv;
 
     DevicePayload::GetService payload;
+    std::cout << "Payload constructed, calling Device::GetService" << std::endl;
     Device::GetService(payload);
-//    boost::asio::io_service ios;
-//    BoostUdp client (ios, "localhost", "1337");
-
-//    client.Send("Hello world\r\n");
 
     return 0;
 }
