530b30e07d4793c5e71f7e25ca1231ee5017b9f6
Kieron Woodhouse <kieron.woodhouse@yahoo.co.uk>
2016-03-09 15:49:55 +0000

    Converted raw pointers to smart pointers.

--- network/NetworkBoost.h
+++ network/NetworkBoost.h
@@ -5,6 +5,7 @@
 #include "Network.h"
 
 #include <boost/asio.hpp>
+#include <boost/scoped_ptr.hpp>
 #include <string>
 
 // Debugging
@@ -14,28 +15,21 @@ class NetworkBoost : public Network
 {
 public:
     NetworkBoost(const std::string& host, const std::string& port)
-              : Network(host, port)
+              : Network(host, port),
+                m_ios(new boost::asio::io_service()),
+                m_boostUdp(new BoostUdp(*m_ios, host, port))
     {
 #ifdef _DEBUG_
         std::cout << "Constructing NetworkBoost" << std::endl;
 #endif
-
-        m_ios = new boost::asio::io_service();
-        m_boostUdp = new BoostUdp(*m_ios, m_host, m_port);
-    }
-
-    ~NetworkBoost()
-    {
-        delete m_ios;
-        delete m_boostUdp;
     }
 
     void Send(const std::string& message);
     std::string Receive();
 
 private:
-    BoostUdp* m_boostUdp;
-    boost::asio::io_service* m_ios;
+    boost::scoped_ptr<boost::asio::io_service> m_ios;
+    boost::scoped_ptr<BoostUdp> m_boostUdp;
 };
 
 #endif // NETWORKBOOST_H
